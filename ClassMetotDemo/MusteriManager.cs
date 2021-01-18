01318e6ea9d79f2cb5bb3b9a2fdd989232d7d906
Orjinal parfumcum <orjinalparfumcum@outlook.com>
2021-01-18 06:29:55 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+       
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+"  "+musteri.Soyad);
+            Console.WriteLine("--------------Müşteri Eklendi-------------------");
+        }
+
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + "  " + musteri.Soyad);
+            //Musteriler.Remove(musteri);
+            Console.WriteLine("--------------Müşteri Silindi-------------------");
+
+        }
+
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (var item in musteriler)
+            {
+                Console.WriteLine(item.Ad + "  " + item.Soyad);
+
+            }
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Musterileri Tanimladim
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "onur";
+            musteri1.Soyad = "Karasu";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Engin";
+            musteri2.Soyad = "Demiroğ";
+
+            Musteri[] musteriler = new Musteri[] { musteri1,musteri2}; //Musteriler Array'ini olustrudum
+
+            MusteriManager musteriManager = new MusteriManager(); // MusteriManager Kısmını Tanimladım
+
+            //Musteri Ekledim
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+
+
+            //Musteri Sildim
+            musteriManager.MusteriSil(musteri1);
+
+            //Musterileri Listeledim
+            musteriManager.MusteriListele(musteriler);
+            
+
+        }
+    }
+}
